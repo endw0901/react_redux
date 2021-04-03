15d3451c8924461a2d4352619b10e22123515b5d
endw <57078225+endw0901@users.noreply.github.com>
2021-04-03 07:48:39 -0900

    Create GuestList.tsx

---
+++ rts/src/state/GuestList.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const GuestList: React.FC = () => {
+  const [name, setName] = useState('');
+  // そのままだとnever[]とtypescriptが解釈するので、string配列と明記する
+  // => 引数に、...guests配列、最後にname(string)、を受けれるようになる
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () => {
+    setName('');
+    setGuests([...guests, name]);
+  };
+  return (
+    <div>
+      <h3>Guist List</h3>
+
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button>Add Guest</button>
+    </div>
+  );
+};
+
+export default GuestList;
