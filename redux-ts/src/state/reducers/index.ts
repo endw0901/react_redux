4e7cd3e991abba74df65ad35aa7a4f57b4eea655
endw <57078225+endw0901@users.noreply.github.com>
2021-04-10 22:10:51 +0900

    Update index.ts

--- redux-ts/src/state/reducers/index.ts
+++ redux-ts/src/state/reducers/index.ts
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
-import repositoriesReducer from './repositoriesReducuer';
+import repositoriesReducer from './repositoriesReducer';
 
+// state, reducerの戻りinterface:RepositoriesStateを理解している
+// => RootStateで渡す
 const reducers = combineReducers({
+  // key:value => RootStateでこのdataを持つことををタイプ定義として渡す
   repositories: repositoriesReducer,
 });
 
 export default reducers;
+
+// redux storeのtype定義を新たに作る
+//   => components側で理解できるようにするため
+export type RootState = ReturnType<typeof reducers>;
