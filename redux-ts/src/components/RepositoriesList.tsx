a3e75af7c135b3142ff42f8150c39e936de7c142
endw <57078225+endw0901@users.noreply.github.com>
2021-04-10 21:17:03 +0900

    Update RepositoriesList.tsx

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+
+// formの値をaction-creatorに渡して、それを引数にdispatch渡す
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     // submitボタンクリック時にページがリフレッシュする挙動をなくす
     event.preventDefault();
+
+    // formの値をaction-creatorに渡して、それを引数にdispatch渡す
+    // dispatch(actionCreators.searchRepositories(term)); => これをする代わりにhookを使う
+    searchRepositories(term);
   };
 
   return (
