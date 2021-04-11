48d403786ca30093af33c7e2f21ab69acaacdd30
endw <57078225+endw0901@users.noreply.github.com>
2021-04-11 22:38:01 +0900

    Update RepositoriesList.tsx

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+// import { useSelector } from 'react-redux';
+// state/reducersのtypeを理解するためのRootStateを使ったuseSelector hook
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
 
 // formの値をaction-creatorに渡して、それを引数にdispatch渡す
@@ -6,6 +9,13 @@ import { useActions } from '../hooks/useActions';
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+
+  // state/reducersのreturnにアクセスできる(error, success, payload)
+  // state/reducersのtype理解のため、reducuers/indexで新しいtypeを作る
+  //  => useTypedSelector hook => stateがreducerのkey valueを理解するようになる
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     // submitボタンクリック時にページがリフレッシュする挙動をなくす
@@ -22,6 +32,10 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {/* errorがnullでないとき(null or stringなので) */}
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data}
     </div>
   );
 };
